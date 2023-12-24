1b80f67df5d53120faf233275ba67b4cb0f5eff8
jayvee-pearlpay <jaybee291998@gmail.com>
2023-12-24 09:33:31 +0800

    initial commit

---
+++ src/main/java/com/jaybee291998/mangadex/mangadex_web_be/MangadexWebBeApplication.java
@@ -0,0 +1,13 @@
+package com.jaybee291998.mangadex.mangadex_web_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangadexWebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangadexWebBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaybee291998/mangadex/mangadex_web_be/MangadexWebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaybee291998.mangadex.mangadex_web_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangadexWebBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
